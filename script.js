19523b0e1d5b32ae7cfc859e77a316df2f0dd08d
Quentin Plet <quentin.plet@gmail.com>
2024-10-13 15:45:15 +0200

    C2 : extraction des constantes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+import constantes from "./constantes.js";
+
+const dialogTitle = document.querySelector(".dialog__title");
+const dialogText = document.querySelector(".dialog__text");
+
 const dialog = document.querySelector("#dialog");
 const closeBtn = document.querySelector(".dialog__close");
 
-window.onload = () => {
+window.addEventListener("load", () => {
+  dialogTitle.textContent = constantes.HELLO_WORLD_TITLE;
+  dialogText.textContent = constantes.HELLO_WORLD_TEXT;
   dialog.showModal();
-};
+});
 
 closeBtn.addEventListener("click", () => {
   dialog.close();
